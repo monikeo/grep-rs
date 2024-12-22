68b8f5e55662a7950048a8bd06f98ffd8d5879a1
monikeo281000 <dark.moni.281000@gmail.com>
2024-12-22 11:25:22 +0700

    progess cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -10,22 +10,52 @@ use clap::Parser;
 pub struct Args {
     #[arg(short, long, help="The pattern to search for")]
     pattern: Option<String>,
-    #[arg(short, long, help="The files to search")]
-    files: Option<String>,
-    #[arg(short, 
+    #[arg(short='f', long="file", help="The file to search")]
+    file: Option<String>,
+    #[arg(short='F', long="files", help="The files to search")]
+    files: Option<Vec<String>>,
+    #[arg(short='i', 
         long="ignore-case", 
+        default_value="false",
         help="Ignore case distinctions")]
-    ignore_case: Option<String>,
+    ignore_case: bool,
     #[arg(short='r',
         long="recursive",
+        default_value="false",
         help="Search directories recursively")]
-    recursive: Option<String>,
+    recursive: bool,
     #[arg(short='n',
         long="line-number",
+        default_value="false",
         help="Show line numbers with output lines")]
-    line_number: Option<String>,
+    line_number: bool,
     #[arg(short='v',
         long="invert-match",
+        default_value="false",
         help="Invert the match")]
-    inivert_match: Option<String>
+    invert_match: bool 
+}
+
+impl Args {
+    pub fn get_pattern(&self) -> Option<&String> {
+        self.pattern.as_ref()
+    }
+    pub fn get_file(&self) -> Option<&String> {
+        self.file.as_ref()
+    }
+    pub fn get_files(&self) -> Option<&Vec<String>> {
+        self.files.as_ref()
+    }
+    pub fn get_ignore_case(&self) -> bool {
+        self.ignore_case
+    }
+    pub fn get_recursive(&self) -> bool {
+        self.recursive
+    }
+    pub fn get_line_number(&self) -> bool {
+        self.line_number
+    }
+    pub fn get_invert_match(&self) -> bool {
+        self.invert_match
+    }
 }
