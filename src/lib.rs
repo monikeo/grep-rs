af016a3ac6edb9758bb494643b760e23120d8ec7
monikeo281000 <dark.moni.281000@gmail.com>
2024-12-31 01:06:26 +0700

    working on main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,41 @@
-use std::process;
 pub mod cli;
 pub mod file;
 pub mod model;
 pub mod search;
 pub mod recursive;
-use model::Config;
+use cli::Args;
+use file::FileHandling;
+use recursive::find_files_recursively;
+
+use std::process;
+use std::path::PathBuf;
 
 pub fn error_exit(message: &str) {
     println!(" [-] ERROR: {}", message);
     process::exit(0x0100);
+}
+
+pub fn get_file_paths(args: &Args) -> Vec<PathBuf>{
+    if args.get_recursive() && args.get_directory().is_some() {
+        if args.get_file().is_some() || args.get_files().is_some() {
+            let message = "You cannot specify both file search and recursive at the same time.";
+            error_exit(message);
+        }
+        let directory = args.get_directory().unwrap();
+        if FileHandling::is_dir(directory) {
+            find_files_recursively(directory)
+        } else {
+            Vec::new()
+        }
+    } else if let Some(file) = args.get_file() {
+        vec![file.into()]
+    } else if let Some(files) = args.get_files() {
+        files.into_iter().map(Into::into).collect()
+    } else {
+        let message = "No file or files specified. Use `--file` or `--files` to provide input.";
+        error_exit(message);
+        Vec::new()
+    }
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,13 @@ mod file;
 mod search;
 mod recursive;
 mod model;
-use file::{FileError, FileHandling};
-use cli::Args;
-use recursive::find_files_recursively;
 use model::Config;
 use search::search_lines;
 
 use grep_rs::*;
+use grep_rs::cli::Args;
 
-use clap::Parser;
+use std::path::PathBuf;
 
 fn run(args: Args) {
     let query = args.get_pattern();
@@ -21,41 +19,21 @@ fn run(args: Args) {
         error_exit(message);
     }
 
-
-    let files = if args.get_recursive() && args.get_directory().is_some() {
-        if args.get_file().is_some() || args.get_files().is_some() {
-            let message = "You cannot specify both file search and recursive at the same time.";
-            error_exit(message);
-        }
-        let directory = args.get_directory().unwrap();
-        if FileHandling::is_dir(directory) {
-            find_files_recursively(directory)
-        } else {
-            Vec::new()
-        }
-    } else if let Some(file) = args.get_file() {
-        vec![file.into()]
-    } else if let Some(files) = args.get_files(){
-        files.into_iter().map(Into::into).collect()
-    } else {
-        let message = "No file or files specified. Use `--file` or `--files` to provide input.";
-        error_exit(message);
-        Vec::new()
-    };
-
-    let mut config = Config::new(query, files);
-    config.set_recursive(args.get_recursive());
-    config.set_ignore_case(args.get_ignore_case());
-    config.set_line_number(args.get_line_number());
-    config.set_invert_match(args.get_invert_match());
-
+    let files = get_file_paths(&args);
+    let config = build_config(&args, query, files);
     let result = search_lines(&config);
 
     println!("{:#?}", result);
-
 }
 
-
+fn build_config(args: &Args, query: &str, files: Vec<PathBuf> ) -> Config {
+    let mut config = Config::new(query, files);
+    config.set_recursive(args.get_recursive()) ;
+    config.set_ignore_case(args.get_ignore_case());
+    config.set_line_number(args.get_line_number());
+    config.set_invert_match(args.get_invert_match());
+    config
+}
 
 fn main() {
     let args = Args::parse_args();
